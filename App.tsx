ca4c9b7d414177a7a542ff97b564fc39ad416fb8
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-10-17 09:49:25 -0600

    Add configuration for custom theme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed'
-import { config } from '@gluestack-ui/config'
+import { config } from '@/config/gluestack-ui.config'
 
 export default function App() {
   return (
