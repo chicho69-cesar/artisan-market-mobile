475e0ac9e214adb8d4276bd5acc195b83f317726
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-10-29 11:20:28 -0600

    Add chats services

---
+++ src/modules/chats/services/index.ts
@@ -0,0 +1,63 @@
+import { api } from '@/config/api'
+
+export async function sendMessage(userToSend: number, message: string, token: string) {
+  try {
+    const { data } = await api.post(
+      '/users/send-message',
+      {
+        user_to_send_message: userToSend,
+        message,
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio: ${error}`)
+    return null
+  }
+}
+
+export async function getConversation(userId: number, token: string) {
+  try {
+    const { data } = await api.get(
+      `/users/conversation/${userId}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    )
+
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio: ${error}`)
+    return null
+  }
+}
+
+export async function getMyConversations(token: string) {
+  try {
+    const { data } = await api.get(
+      '/users/my-conversations',
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    )
+
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio: ${error}`)
+    return null
+  }
+}
